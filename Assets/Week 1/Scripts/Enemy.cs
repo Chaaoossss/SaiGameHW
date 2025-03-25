c7df2561a081f9c3a734b415e0e509c1f6ee1729
Trung Đào <dvtrung93@gmail.com>
2025-03-26 00:36:48 +0700

    + update

--- Assets/Week 1/Scripts/Enemy.cs
+++ Assets/Week 1/Scripts/Enemy.cs
@@ -5,28 +5,66 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    private void Start()
+    {
+        EnemyName();
+    }
+
+    private void Update()
+    {
+        IsDead();
+        RegenHP();
+    }
+
     // 5 Enemies variables
 
     //variables 1 
-
+    protected int maxHealth = 100;
     //variables 2
-
+    protected int healthRegen = 1;
     //variables 3 
-
+    protected int currentHealth = 80;
     //variables 4 
-
+    protected float regenTimer = 0f;
     //variables 5 
-
-
+    protected bool madNessMode = false;
     // 5 Enemies methods
 
     //Method 1
-
+    protected void IsDead()
+    {
+        if (currentHealth <= 0)
+        {
+            Debug.Log(transform.name + " is dead");
+        }
+    }
     //Method 2
-
+    protected void RegenHP()
+    {
+        if (currentHealth < maxHealth)
+        {
+            regenTimer += Time.deltaTime;
+            if (regenTimer >= 1f)
+            {
+                currentHealth += healthRegen;
+                if (currentHealth > maxHealth)
+                {
+                    currentHealth = maxHealth;
+                }
+                regenTimer = 0f;
+            }
+        }
+    }
     //Method 3
-
+    protected void EnemyName()
+    {
+        Debug.Log("Enemy name: " + transform.name);
+    }
     //Method 4
-
+    protected void RandomStatus()
+    {
+        madNessMode = Random.Range(0, 2) == 0 ? false : true;
+        Debug.Log("Madness mode: " + madNessMode);
+    }
     //Method 5
 }
